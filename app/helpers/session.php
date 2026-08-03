57194e0d1032d1b1b5b229e62c7702a84a9a2fc2
kumararvind781 <kumar.arvind781@gmail.com>
2026-08-03 15:59:26 +0530

    .htaccess file update

--- app/helpers/session.php
+++ app/helpers/session.php
@@ -1,6 +1,15 @@
 <?php
 
 if (session_status() === PHP_SESSION_NONE) {
+
+    session_set_cookie_params([
+        'lifetime' => 0,
+        'path' => '/',
+        'secure' => (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'),
+        'httponly' => true,
+        'samesite' => 'Lax'
+    ]);
+
     session_start();
 }
 
